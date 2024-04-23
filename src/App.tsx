c899c39f7161577eae450c9127fe7a3091cdf055
Silalieva Begimay <begimayomurovna@gmail.com>
2024-04-23 11:59:50 +0600

    Первоначальный коммит

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Button } from 'react-bootstrap';
+
+const App = () => {
+    return (
+        // <Button variant="primary">Web Dev Blog</Button>
+    );
+};
+
+export default App;
